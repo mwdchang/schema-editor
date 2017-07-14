dd667e935e223c10a6fbcdc0df902c6c8eacce2d
Daniel <mwdchang@gmail.com>
2017-07-13 21:47:28 -0400

    add error display, add interaction to jumpt to line, use solarized-dark theme, various small tweaks

--- validator.js
+++ validator.js
@@ -6,7 +6,14 @@ class Validator {
   validate(txt) {
     return new Promise( (resolve, reject) => {
       let blah = txt;
-      resolve(blah.replace(/var/g, 'let'));
+      resolve({
+        output: blah.replace(/var/g, 'let'),
+        errors: [
+          {code:123, line:11, txt: 'this is an error'},
+          {code:123, line:24, txt: 'this is an error'},
+          {code:223, line:4, txt: 'this is an error'}
+        ]
+      });
     });
   }
 
